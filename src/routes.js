1944cc361233d785fbd9667d4cf97ca6a01b7be1
Harbs <harbsprog@gmail.com>
2019-08-20 13:11:53 -0300

    Layout Starts

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,6 @@ const express = require('express');
 
 const routes = express.Router()
 
-routes.get('/', (req, res) => {
-  return res.send('Hello World!')
-})
+routes.get('/', (req, res) => res.render('auth/signup'))
 
 module.exports = routes
